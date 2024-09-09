8e7cb845141b6928fa3ff44ed27ef53b88fcee61
Andrew Rathbun <andrew.d.rathbun@gmail.com>
2024-09-08 22:26:05 -0400

    update nuget

--- obj/Debug/net6.0/Wmi-Parser.AssemblyInfo.cs
+++ obj/Debug/net6.0/Wmi-Parser.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wmi-Parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42c079cd316e809e273f6aa20848e98556ae3c4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wmi-Parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wmi-Parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
